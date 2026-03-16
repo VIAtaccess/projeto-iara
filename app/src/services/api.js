9fad224a4d6613bbea5490e413a533483235cc39
VIAtaccess <viataccess@gmail.com>
2026-03-16 11:09:18 -0300

    Update api.js

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-   
-    baseURL: 'http://localhost:3000', 
+    // baseURL: 'http://localhost:3000', <- O endereço antigo e fixo foi removido
+    
+    // O endereço inteligente: ele descobre sozinho se está no Railway ou no seu PC!
+    baseURL: window.location.origin, 
 });
-
 
 api.interceptors.request.use(async (config) => {
     const token = localStorage.getItem('token'); 
